aca8eb985e2ed63a1b8d6817331f3d65de7ba323
Marco Gesteira <mgesteira@outlook.com>
2024-04-10 14:49:08 -0300

    Adicionada a classe Context

---
+++ CartinhasSempreCrianca.Repository/CartinhasSempreCriancaContext.cs
@@ -0,0 +1,42 @@
+﻿using CartinhasSempreCrianca.Domain.Apadrinhamento;
+using CartinhasSempreCrianca.Domain.Cartinha;
+using CartinhasSempreCrianca.Domain.Conta;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartinhasSempreCrianca.Repository
+{
+    public class CartinhasSempreCriancaContext : DbContext
+    {
+        public DbSet<Cartinha> Cartinhas {  get; set; }
+        public DbSet<Instituicao> Instituicoes { get; set; }
+        public DbSet<Diretor> Diretores { get; set; }
+        public DbSet<Crianca> Criancas { get; set;}
+        public DbSet<Padrinho> Padrinhos { get; set; }
+        public DbSet<Moderador> Moderadores { get; set;}
+        public DbSet<LocalDeRecolhimento> LocaisDeRecolhimento { get; set; }
+        public DbSet<Apadrinhamento> Apadrinhamentos { get; set; }
+
+        public CartinhasSempreCriancaContext(DbContextOptions<CartinhasSempreCriancaContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CartinhasSempreCriancaContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLoggerFactory(LoggerFactory.Create(x => x.AddConsole()));
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
